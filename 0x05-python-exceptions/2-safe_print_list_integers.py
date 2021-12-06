0565059cdeb2a03c62b970277dfed00309ca823c
Abrham Bunaro <abrishbunaro@gmail.com>
2021-12-06 12:56:37 -0800

    quick fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[elem]), end="")
             val += 1
         except (ValueError, TypeError):
-            continue
+            pass
     print(end="\n")
-    return val
+    return (val)
 
