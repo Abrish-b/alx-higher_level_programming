a1883b7b00de08f6ff3bdf7373d1adf660a942cb
Abrham Bunaro <abrishbunaro@gmail.com>
2021-12-06 12:52:02 -0800

    safely print ints

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    val = 0
+    for elem in range(0, x):
+        try:
+            print("{:d}".format(my_list[elem]), end="")
+            val += 1
+        except (ValueError, TypeError):
+            pass
+    print(end="\n")
+    return val
+
