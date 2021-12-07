2e89afabeba5e908c32af1fad96ba4ed63fefaea
Abrham Bunaro <abrishbunaro@gmail.com>
2021-12-07 03:50:04 -0800

    alternative solution

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value), end="\n")
         return True
     except (TypeError, ValueError) as Err:
-        sys.stderr.write("Excepion: {} \n".format(Err))
+        sys.stderr.write("Excepion: {:d} \n".format(Err))
         return False
